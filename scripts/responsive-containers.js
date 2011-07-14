c4c7cc7eebaf6d2a1aeac35e2d397d7b3a97cf26
Andy Hume <andyhume@gmail.com>
2011-07-14 17:40:11 +0100

    Initial demo.

---
+++ scripts/responsive-containers.js
@@ -0,0 +1,89 @@
+(function(win) {
+    
+    var doc = win.document,
+        els = [],
+        loaded = false;
+    
+    function findContainerQueries() {
+        // Find data-cq attributes.
+        if (doc.querySelectorAll) {
+            els = document.querySelectorAll("[data-cq]");
+        } else {
+            // If no query selectors.
+            var e = document.getElementsByTagName("*");
+            for (var i = 0, j = e.length; i<j; ++i) {
+                if (e[i].getAttribute("data-cq")) {
+                    els.push(e[i]);
+                }
+            }
+        }
+        //alert(els);
+        // Parse the data-cq attribute and store resulting rules on the element.
+        for (var i = 0, j = els.length; i<j; ++i) {
+            var el = els[i];
+            var cq_rules = [];
+            //alert(el.className);
+            var raw_rules = el.getAttribute("data-cq").split(" ");
+            for (var k = 0, l = raw_rules.length; k<l; ++k) {
+                cq_rules.push(raw_rules[k].split(":"));
+            }
+            el.cq_rules = cq_rules;
+        }
+    }
+    
+    function applyRules() {
+        // For each element we found, apply the rules to the class name.
+        for (var i = 0, j = els.length; i<j; ++i) {
+            el = els[i];
+            for (var k = 0, l = el.cq_rules.length; k<l; ++k) {
+                var rule = el.cq_rules[k];
+                var compareFunction = getCompareFunction(rule[0]);
+                if ( compareFunction(el.offsetWidth, parseInt(rule[1])) ) {
+                    if (el.className.indexOf(rule[2]) < 0) {
+                        el.className += " " + rule[2];
+                    }
+                } else {
+                    el.className = (" " + el.className + " ").replace(" " + rule[2] + " ", " ");
+                }
+            }
+        }
+    }
+    
+    function getCompareFunction(type) {
+        switch(type) {
+            case "min-width":
+                return function(a, b) {
+                    return a > b;
+                }
+                break;
+            case "max-width":
+                return function(a, b) {
+                    return a < b;
+                }
+                break;
+        }
+    }
+    
+    function contentReady() {
+        if (loaded) {
+            return;
+        }
+        loaded = true;
+        findContainerQueries();
+        applyRules();
+        // win.addEventListener("resize", applyRules, false);
+    }
+    
+    if (doc.addEventListener) {
+        document.addEventListener("DOMContentLoaded", contentReady, false);
+        // or
+        win.addEventListener("load", contentReady, false);
+    }
+    // If old IE
+    else if (doc.attachEvent) {
+        doc.attachEvent("onreadystatechange", contentReady);
+        // or
+        win.attachEvent("onload", contentReady);
+    }
+    
+})(this);
